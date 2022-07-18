4c79606d866010c98c13bcbdbba8d64e7cd6f14f
osama-kh1122 <awanosama628@gmail.com>
2022-07-18 10:10:16 +0500

    helpdesk changes

--- helpdesk_subcategories/models/models.py
+++ helpdesk_subcategories/models/models.py
@@ -12,7 +12,7 @@ class HelpdeskTicketTypeExtend(models.Model):
     subtype_id = fields.Many2one('helpdesk.ticket.subtype',string='Subtype', domain=[('type_id', '=', 'ticket_type_id')], ondelete='cascade')
     item_id = fields.Many2one('helpdesk.ticket.item',string='Item', domain=lambda self: [('subtype_id', '=', self.subtype_id.id)], ondelete='cascade')
 
-    @api.onchange('ticket_type_id') 
+    @api.onchange('ticket_type_id')
     def _onchange_ticket_type_id(self):
         return {'domain': {'subtype_id': [('type_id', '=', self.ticket_type_id.id)]}}
 
@@ -22,7 +22,7 @@ class HelpdeskTicketTypeExtend(models.Model):
 
 class HelpdeskSubtype(models.Model):
     _name = 'helpdesk.ticket.subtype'
-    _description = ''
+    _description = '  '
 
     sequence = fields.Integer(default=10)
     name = fields.Char('name', required=True)
